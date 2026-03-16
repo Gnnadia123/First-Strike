0d156cd11e5880694fc035530c128b88ae1c7ed0
Robotics @APEX <aidanng23@gmail.com>
2026-03-16 11:26:49 +0800

    api update

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import random
 from datetime import datetime, timezone
 import json
 
-with open("moves.json", "r") as f:
-    moves = json.load(f)
+with open('/Users/aidanng/Desktop/Desktop Items/Code/First Strike/First Strike/moves.json', "r") as f:
+    moves_data = json.load(f)
+
+moves = list(moves_data.values())
 
 now = datetime.now(timezone.utc)
 
@@ -16,13 +18,14 @@ shop = []
 
 for i in range(3):
     shop.append(random.choice(moves))
+
 app = fastapi.FastAPI()
 
 @app.get("/")
 def read_root():
     return {"shop": {
-            "item 1" : shop[0],
-            "item 2" : shop[1],
-            "item 3" : shop[2]
+            "item 1" : shop[0]["code"],
+            "item 2" : shop[1]["code"],
+            "item 3" : shop[2]["code"]
             }
         }
