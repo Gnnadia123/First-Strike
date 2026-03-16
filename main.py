b1cc9c55bde279eaf4f8b5ee29769117e772c2e8
Robotics @APEX <aidanng23@gmail.com>
2026-03-16 10:39:09 +0800

    api test

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 import fastapi
+import random
+from datetime import datetime, timezone
+import json
 
+with open("moves.json", "r") as f:
+    moves = json.load(f)
+
+now = datetime.now(timezone.utc)
+
+hour = now.hour
+random.seed(hour)
+
+shop = []
+
+for i in range(3):
+    shop.append(random.choice(moves))
 app = fastapi.FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"idwusyagshus": {"asdasd": "asdasd"}}
+    return {shop[0]["code"]: shop[0], shop[1]["code"]: shop[1], shop[2]["code"]: shop[2]}
