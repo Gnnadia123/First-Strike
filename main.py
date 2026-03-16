41a3478e220c5b905b4bca1739ccface7c47b4cf
Robotics @APEX <aidanng23@gmail.com>
2026-03-16 10:14:58 +0800

    api test

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ app = fastapi.FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"idwusyagshus": "World"}
-    return {"test world": "prive"}
+    return {"idwusyagshus": {"asdasd": "asdasd"}}
